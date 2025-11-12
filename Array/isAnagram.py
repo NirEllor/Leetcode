6f67cf531ba725c2f2901b6db9aefbaabacbf195
Nir Ellor <nirellorwaizner@gmail.com>
2025-11-12 17:45:26 +0200

    lettcoding arrays

--- Array/isAnagram.py
+++ Array/isAnagram.py
@@ -2,7 +2,7 @@ def isAnagram(self, s: str, t: str) -> bool:
     if len(s) != len(t):
         return False
 
-    count = [0] * 26  # מניח רק a-z
+    count = [0] * 26
     for i in range(len(s)):
         count[ord(s[i]) - ord('a')] += 1
         count[ord(t[i]) - ord('a')] -= 1
